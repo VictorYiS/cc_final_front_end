be1fe8a9bc982084bf652866a1394848935ec176
ViktorYiS <905205299@qq.com>
2025-01-07 17:15:20 +0100

    add more

---
+++ static/common.js
@@ -0,0 +1,9 @@
+const urlPrefix = 'https://29de-94-209-250-122.ngrok-free.app';
+
+function showLoading(document) {
+    document.getElementById('loadingIndicator').style.display = 'flex';
+}
+
+function hideLoading(document) {
+    document.getElementById('loadingIndicator').style.display = 'none';
+}
